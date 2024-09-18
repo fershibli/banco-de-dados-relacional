8eb35188445a70d096d970df0e24607a15d89f47
FerShibli <fernandoluiz.1991@gmail.com>
2024-09-17 22:10:29 -0300

    feat(aula07): Adicionar script SQL para criação de tabelas Cidade, Empregado, Funcionario, Pessoa e Pessoa_Fisica

--- Aula07/SQLQuery2.sql
+++ Aula07/SQLQuery2.sql
@@ -11,15 +11,15 @@ INSERT INTO cliente VALUES (0001,'Marcelo Cruz' ,'Rua Brasil, 35'
 
 SELECT 'Código do Cliente', cod_cliente, 'Nome do Cliente',
 nome_cliente
-FROM cliente
+FROM cliente;
 
 SELECT cod_cliente as 'Código do Cliente', nome_cliente as 'Nome
 do Cliente'
-FROM cliente
+FROM cliente;
 
 SELECT num_pedido, cod_produto, quantidade
 FROM item_pedido
-WHERE quantidade = 45
+WHERE quantidade = 45;
 
 create table funcionario(
 nrmatricula int primary key,
@@ -37,19 +37,90 @@ insert into funcionario values(5, 'Pedro', 'Cavalcante', 2, 5000);
 insert into funcionario values(6, 'Paula', 'Ferreira', 1, 6000);
 insert into funcionario values(7, 'Carlos', 'Santana', 3, 1700);
 insert into funcionario values(8, 'Marta', 'Silveira', 2, 1200);
+insert into funcionario values(9, 'Yasmin', 'Santos', 1, 1500);
 
 Select nrmatricula, primeiro_nome, ultimo_nome, cod_setor
 from funcionario
-where cod_setor = 2
+where cod_setor = 2;
 
 Select primeiro_nome, salario
 from funcionario
-where salario < 1500
+where salario < 1500;
 
 Select primeiro_nome, salario
 from funcionario
-where salario between 1500 and 2000
+where salario between 1500 and 2000;
 
 SELECT cod_produto, descricao
 FROM produto
-WHERE valor_unitario between 0.32 and 2.00
+WHERE valor_unitario between 0.32 and 2.00;
+
+Select primeiro_nome, salario, cod_setor
+from funcionario
+where primeiro_nome like '%M%';
+
+Select primeiro_nome, salario, cod_setor
+from funcionario
+where primeiro_nome like 'M%';
+
+SELECT cod_produto, descricao, unidade
+FROM produto
+WHERE unidade like 'K%';
+
+SELECT nome_vendedor
+FROM vendedor
+WHERE faixa_comissao not in ('A', 'B')
+
+Select primeiro_nome, salario, cod_setor
+from funcionario
+where cod_setor in (1,3);
+
+select * from funcionario f
+where cod_setor not in (1,3);
+
+SELECT *
+FROM cliente
+WHERE ie is null;
+
+Select nrmatricula, ultimo_nome, salario
+from funcionario
+where salario > 1000 and nrmatricula >= 3;
+
+Select *
+from funcionario
+where salario > 2500 or cod_setor = 4;
+
+SELECT descricao
+FROM produto
+WHERE unidade = 'LT' and valor_unitario > 51;
+
+SELECT descricao
+FROM produto
+WHERE unidade = 'UN' and valor_unitario = 4;
+
+SELECT nome_vendedor, salario_fixo
+FROM vendedor
+ORDER by nome_vendedor;
+
+Select *
+from funcionario f
+where cod_setor NOT IN (2,4)
+Order by cod_setor DESCSelect *
+from funcionario f
+where cod_setor NOT IN (2,4)
+Order by cod_setor, primeiro_nome
+
+CREATE TABLE PESSOA
+(
+id_pessoa integer primary key,
+nome varchar(20),
+cpf varchar(14)
+);
+
+CREATE TABLE PESSOA_FISICA
+(
+id_pessoa integer primary key,
+nome varchar(20),
+cpf varchar(14)
+);INSERT INTO PESSOA VALUES (1, 'PEDRO CABRAL', '12345678991');INSERT INTO PESSOA_FISICA SELECT ID_PESSOA, NOME, CPF FROM
+PESSOA;
